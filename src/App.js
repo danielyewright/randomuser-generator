1b14ba88c4225ab0b68c35235aafd018153f930e
Daniely Wright <danielywrighte@gmail.com>
2019-10-18 05:41:07 -0400

    Fix responsiveness on mobile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="container">
       <div className="row justify-content-center">
-        <div className="col-6">
+        <div className="col-md-6 col-sm-12">
           <h1 className="mb-5 mt-3">Random User Generator</h1>
           <p>This tool gets random user data from the <a href="https://randomuser.me" target="_blank" rel="noopener noreferrer">randomuser.me</a> API, which can then be downloaded as a CSV or JSON file.</p>
           <p>Once data is generated, you must click <b>Clear</b> or refresh the page to re-generate data.</p>
